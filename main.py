1100ceebaabd67f96aee277555eb2d55773d84dc
CocoGiogio <giovane.bomme@gmail.com>
2025-01-14 15:59:38 +0100

    maj des fichiers

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 from scapy.all import ARP, Ether, srp
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/scan')
+def scan():
     # Network scanning logic
     target_ip = "192.168.1.1/24"  # Change this to your network range
     arp = ARP(pdst=target_ip)
@@ -14,10 +18,10 @@ def home():
     result = srp(packet, timeout=3, verbose=0)[0]
 
     devices = []
-    for sent, received in result:
+    for _, received in result:
         devices.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-    return jsonify(devices)
+    return render_template('scan.html', devices=devices)
 
 if __name__ == '__main__':
     app.run(debug=True)
