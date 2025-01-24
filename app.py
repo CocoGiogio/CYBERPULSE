95e20438b3ebe60b5db910ecb4a1b3b43d56d554
w4rsh3ll <s.mccalla@proton.me>
2025-01-24 16:54:14 +0100

    Création des templates pour chacune des pages

--- app.py
+++ app.py
@@ -8,10 +8,41 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Home page route
+# About page route
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Network Scanner page route
+@app.route('/net_scan')
+def net_scan():
+    return render_template('net_scan.html')
+
+# Port Scanner page route
+@app.route('/port_scan')
+def port_scan():
+    return render_template('port_scan.html')
+
+# Username search page route
+@app.route('/username_search')
+def username_search():
+    return render_template('username_search.html')
+
+# Google Dorking page route
+@app.route('/google_dorking')
+def google_dorking():
+    return render_template('google_dorking.html')
+
+
+# Remediation page route
+@app.route('/remediation')
+def remediation():
+    return render_template('remediation.html')
+
+# Chatbott page route
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
 
 # Run the app
 if __name__ == '__main__':
